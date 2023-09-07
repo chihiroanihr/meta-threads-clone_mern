704c64a38725ed0836a6852a945f92cf92e074e9
Rhina Kim <chihiroanihr@gmail.com>
2023-09-06 21:55:38 -0400

    Minor Update app/auth/onboarding/page.tsx : userData field > value string modified

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -11,7 +11,7 @@ async function Page() {
     id: user?.id,
     objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
+    name: userInfo?.name || `${user?.firstName} ${user?.lastName}` || "",
     bio: userInfo?.bio || "",
     image: userInfo?.image || user.imageUrl,
   };
