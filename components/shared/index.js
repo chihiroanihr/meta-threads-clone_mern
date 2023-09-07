a49283688e411fe97ef290d7f8ca5e933639012a
Rhina Kim <chihiroanihr@gmail.com>
2023-09-07 17:13:53 -0400

    Update /components/shared/index.js added some components to exports.

--- components/shared/index.js
+++ components/shared/index.js
@@ -2,5 +2,14 @@ import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
 
-export { Topbar, LeftSidebar, RightSidebar, Bottombar };
+export {
+  Topbar,
+  LeftSidebar,
+  RightSidebar,
+  Bottombar,
+  ProfileHeader,
+  ThreadsTab,
+};
