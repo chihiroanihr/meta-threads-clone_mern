e50b80f03d36eaffee17fa0cf591925ef580d777
Rhina Kim <chihiroanihr@gmail.com>
2023-09-06 22:40:22 -0400

    Update lib/validations/user.ts : username & name schema modified

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 // Form Schema
 export const UserValidation = z.object({
   profile_photo: z.string().url().nonempty(),
-  name: z.string().min(3).max(30),
-  username: z.string().min(3).max(30),
+  name: z.string().max(30),
+  username: z.string().nonempty().min(3).max(30),
   bio: z.string().max(1000),
 });
