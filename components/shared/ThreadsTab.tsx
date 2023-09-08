7d53e5b740fc7a0be23b6902747a1d2e3e8186e2
Rhina Kim <chihiroanihr@gmail.com>
2023-09-07 20:11:17 -0400

    Update /components/shared/ThreadsTab.tsx :  no-result case implemented
    TODO: if no result then display blank instead of redirect. --> Complete

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 import { ThreadCard } from "@/components/cards";
 import { fetchUserThreads } from "@/lib/actions/user.actions";
 
@@ -16,36 +14,42 @@ async function ThreadsTab({
 }: ThreadsTabProps) {
   // Fetch all threads for "clicked" user via  its user ID (Call to backend)
   let result = await fetchUserThreads(userId);
-  /* TODO: if no result then display blank instead of redirect. */
-  if (!result) redirect("/");
 
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {result.threads.map((thread: any) => (
-        <ThreadCard
-          key={thread._id}
-          id={thread._id}
-          currentUserId={currentUserId}
-          parentId={thread.parentId}
-          content={thread.text}
-          author={
-            accountType === "User"
-              ? {
-                  id: result.id,
-                  username: result.username,
-                  image: result.image,
-                }
-              : {
-                  id: thread.author.id,
-                  username: thread.author.username,
-                  image: thread.author.image,
-                }
-          } // TODO
-          community={thread.community} // TODO
-          createdAt={thread.createdAt}
-          comments={thread.children}
-        />
-      ))}
+      {
+        // If no threads exist
+        result.threads.length === 0 ? (
+          <p className="no-result">No users found.</p>
+        ) : (
+          // If threads exist
+          result.threads.map((thread: any) => (
+            <ThreadCard
+              key={thread._id}
+              id={thread._id}
+              currentUserId={currentUserId}
+              parentId={thread.parentId}
+              content={thread.text}
+              author={
+                accountType === "User"
+                  ? {
+                      id: result.id,
+                      username: result.username,
+                      image: result.image,
+                    }
+                  : {
+                      id: thread.author.id,
+                      username: thread.author.username,
+                      image: thread.author.image,
+                    }
+              } // TODO
+              community={thread.community} // TODO
+              createdAt={thread.createdAt}
+              comments={thread.children}
+            />
+          ))
+        )
+      }
     </section>
   );
 }
