8e606564a4deb1579175b3728be88abd74aebe5a
Rhina Kim <chihiroanihr@gmail.com>
2023-09-09 10:37:35 -0400

    Update /lib/utils.ts : followed typescript rules

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,20 +12,21 @@ export function isBase64Image(imageData: string) {
 }
 
 export function formatDateString(dateString: string) {
-  const options: Intl.DateTimeFormatOptions = {
+  const date = new Date(dateString);
+
+  const formattedDate = date.toLocaleDateString(undefined, {
     year: "numeric",
     month: "short",
     day: "numeric",
-  };
+  });
 
-  const date = new Date(dateString);
-  const formattedDate = date.toLocaleDateString(undefined, options);
-
-  const time = date.toLocaleTimeString([], {
+  const formattedTime = date.toLocaleTimeString([], {
     hour: "numeric",
     minute: "2-digit",
   });
 
-  return `${time} — ${formattedDate}`;
+  const newDateString = `${formattedTime} — ${formattedDate}`;
+
+  return newDateString;
 }
 }
