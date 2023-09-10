f03d7fbaff399939909c236a3df91152b007c2f8
Rhina Kim <chihiroanihr@gmail.com>
2023-09-09 20:54:19 -0400

    Update mongoose.ts : env variable retrieval modified

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,16 +3,30 @@ import mongoose from "mongoose";
 let isConnected = false; // Check connection status
 
 export const connectToDB = async () => {
-  if (!process.env.MONGODB_URL)
-    return console.log("[LOG] MONGODB_URL not found.");
+  const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+  const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+  const MONGODB_CLUSTER_NAME = process.env.MONGODB_CLUSTER_NAME;
+  const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
 
+  // If connection URL invalid
+  if (!MONGODB_USERNAME || !MONGODB_PASSWORD || MONGODB_CLUSTER_NAME)
+    throw new Error("[LOG] Error: MongoDB connection URL not found.");
+
+  // If already connected to the DB
   if (isConnected) return console.log("[LOG] Already connected to MongoDB.");
 
-  mongoose.set("strictQuery", true); // Prevent unknown field queries
+  // DB connection URL
+  const MONGODB_CONN_URL =
+    `mongodb+srv://${MONGODB_USERNAME}:` +
+    `${MONGODB_PASSWORD}@${MONGODB_CLUSTER_NAME}.w3vcjr6.mongodb.net/` +
+    `${MONGODB_DB_NAME}?retryWrites=true&w=majority`;
+
+  // Prevent unknown field queries
+  mongoose.set("strictQuery", true);
 
   try {
-    // Connect
-    await mongoose.connect(process.env.MONGODB_URL);
+    // Connect to the DB
+    await mongoose.connect(MONGODB_CONN_URL);
     isConnected = true;
     console.log("[LOG] Connected to MongoDB.");
   } catch (error) {
