97012fec57ad018e47a64056ad0659eb9645a4f3
Rhina Kim <chihiroanihr@gmail.com>
2023-09-12 16:01:08 -0400

    Update route.ts : Webhook event handlers methods modified

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -24,7 +24,8 @@ import {
   removeUserFromCommunity,
   updateCommunityInfo,
 } from "@/lib/actions/community.actions";
-import console from "console";
+
+let communityJustCreated = false;
 
 /**
  * Backend endpoint setup to receive the webhook events.
@@ -38,7 +39,6 @@ export const POST = async (request: Request) => {
     const svix_id = header.get("svix-id");
     const svix_timestamp = header.get("svix-timestamp");
     const svix_signature = header.get("svix-signature");
-
     // If there are no headers, error out
     if (!svix_id || !svix_timestamp || !svix_signature) {
       console.log("[LOG] Error: No svix headers.");
@@ -77,7 +77,6 @@ export const POST = async (request: Request) => {
     const webhook = new Webhook(webhookSecret);
 
     let evnt: WebhookEvent | null = null;
-
     // Verify the authenticity of the webhook headers + payload (important for security).
     try {
       evnt = webhook.verify(
@@ -101,132 +100,158 @@ export const POST = async (request: Request) => {
 
     // Get the event type
     const eventType = evnt.type;
-
-    // Handle event
-    switch (eventType) {
-      // Handle the webhook: Listen organization creation event
-      case "organization.created":
-        // Retrieve webhook response data &
-        // Insert/Create a community (Call to backend)
-        await createCommunity({
-          communityId: evnt.data.id,
-          communityName: evnt.data.name,
-          communitySlug: String(evnt.data.slug),
-          communityImage: evnt.data.image_url,
-          communityBio: "org bio",
-          createdByUserId: evnt.data.created_by,
-        });
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Community created." },
-          { status: 201 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/CreateOrganization
-      Show what evnt?.data sends from above resource
-      */
-
-      // Handle the webhook: Listen organization invitation creation event.
-      /*
-      Just to show. You can avoid this
-      or tell people that we can create a new mongoose action and add pending invites in the database.
-      */
-      case "organizationInvitation.created":
-        // Retrieve webhook response data
-        const res = evnt.data || {};
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Invitation created." },
-          { status: 201 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Invitations#operation/CreateOrganizationInvitation
-      Show what evnt?.data sends from above resource
-      */
-
-      // Handle the webhook: Listen organization membership (member invite & accepted) creation
-      case "organizationMembership.created":
-        // Retrieve webhook response data &
-        // Insert/Create a member to the community based on its community ID and the member's user ID (Call to backend)
-        await addMemberToCommunity({
-          communityId: evnt.data.organization.id,
-          userId: evnt.data.public_user_data.user_id,
-        });
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Invitation accepted." },
-          { status: 201 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/CreateOrganizationMembership
-      Show what evnt?.data sends from above resource
-      */
-
-      // Handle the webhook: Listen member deletion event
-      case "organizationMembership.deleted":
-        // Retrieve webhook response data &
-        // Delete a member from the community based on its community ID and the member's user ID (Call to backend)
-        await removeUserFromCommunity({
-          communityId: evnt.data.organization.id,
-          userId: evnt.data.public_user_data.user_id,
-        });
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Member removed." },
-          { status: 200 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/DeleteOrganizationMembership
-      Show what evnt?.data sends from above resource
-      */
-
-      // Handle the webhook: Listen organization updation event
-      case "organization.updated":
-        // Retrieve webhook response data &
-        // Update the community info via community ID (Call to backend)
-        await updateCommunityInfo({
-          communityId: evnt.data.id,
-          communityName: evnt.data.name,
-          communitySlug: String(evnt.data.slug),
-          communityImage: evnt.data.image_url,
-        });
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Community updated." },
-          { status: 200 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/UpdateOrganization
-      Show what evnt?.data sends from above resource
-      */
-
-      // Handle the webhook: Listen organization deletion event
-      case "organization.deleted":
-        // Retrieve webhook response data &
-        // Delete the community info via community ID (Call to backend)
-        await deleteCommunity(String(evnt.data.id));
-
-        // Send the response
-        return NextResponse.json(
-          { message: "Community deleted." },
-          { status: 200 }
-        );
-      /*
-      Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/DeleteOrganization
-      Show what evnt?.data sends from above resource
-      */
+    if (!eventType) {
+      console.log("[LOG] Unknown event type");
+      return NextResponse.json(
+        { message: "Unknown event type" },
+        { status: 400 }
+      );
     }
 
-    console.log("[LOG] Unknown event type:", eventType);
-    return NextResponse.json(
-      { message: "Unknown event type" },
-      { status: 400 }
-    );
+    // Different response for newly created community
+    if (communityJustCreated) {
+      /*
+      The flow (API callstack) of the organization creation event from the clerk:
+      1. organization.created ->
+      2. organizationMembership.created ->
+      3. organization.updated
+      */
+      if (
+        eventType === "organizationMembership.created" ||
+        eventType === "organization.updated"
+      ) {
+        communityJustCreated = false;
+        // Send the response
+        return NextResponse.json(
+          { message: "Execiton Skipped." },
+          { status: 200 }
+        );
+      }
+    } else {
+      // Handle event
+      switch (eventType) {
+        // Handle the webhook: Listen organization creation event
+        case "organization.created":
+          communityJustCreated = true;
+
+          // Retrieve webhook response data &
+          // Insert/Create a community (Call to backend)
+          await createCommunity({
+            communityId: evnt.data.id,
+            communityName: evnt.data.name,
+            communitySlug: String(evnt.data.slug),
+            communityImage: evnt.data.image_url,
+            communityBio: "org bio",
+            createdByUserId: evnt.data.created_by,
+          });
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Community created." },
+            { status: 201 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/CreateOrganization
+        Show what evnt?.data sends from above resource
+        */
+
+        // Handle the webhook: Listen organization invitation creation event.
+        /*
+        Just to show. You can avoid this
+        or tell people that we can create a new mongoose action and add pending invites in the database.
+        */
+        case "organizationInvitation.created":
+          // Retrieve webhook response data
+          const res = evnt.data || {};
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Invitation created." },
+            { status: 201 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Invitations#operation/CreateOrganizationInvitation
+        Show what evnt?.data sends from above resource
+        */
+
+        // Handle the webhook: Listen organization membership (member invite & accepted) creation
+        case "organizationMembership.created":
+          // Retrieve webhook response data &
+          // Insert/Create a member to the community based on its community ID and the member's user ID (Call to backend)
+          await addMemberToCommunity({
+            communityId: evnt.data.organization.id,
+            userId: evnt.data.public_user_data.user_id,
+            admin: evnt.data.role === "admin",
+          });
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Invitation accepted." },
+            { status: 201 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/CreateOrganizationMembership
+        Show what evnt?.data sends from above resource
+        */
+
+        // Handle the webhook: Listen member deletion event
+        case "organizationMembership.deleted":
+          // Retrieve webhook response data &
+          // Delete a member from the community based on its community ID and the member's user ID (Call to backend)
+          await removeUserFromCommunity({
+            communityId: evnt.data.organization.id,
+            userId: evnt.data.public_user_data.user_id,
+            admin: evnt.data.role === "admin",
+          });
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Member removed." },
+            { status: 200 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/DeleteOrganizationMembership
+        Show what evnt?.data sends from above resource
+        */
+
+        // Handle the webhook: Listen organization updation event
+        case "organization.updated":
+          // Retrieve webhook response data &
+          // Update the community info via community ID (Call to backend)
+          await updateCommunityInfo({
+            communityId: evnt.data.id,
+            communityName: evnt.data.name,
+            communitySlug: String(evnt.data.slug),
+            communityImage: evnt.data.image_url,
+          });
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Community updated." },
+            { status: 200 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/UpdateOrganization
+        Show what evnt?.data sends from above resource
+        */
+
+        // Handle the webhook: Listen organization deletion event
+        case "organization.deleted":
+          // Retrieve webhook response data &
+          // Delete the community info via community ID (Call to backend)
+          await deleteCommunity(String(evnt.data.id));
+
+          // Send the response
+          return NextResponse.json(
+            { message: "Community deleted." },
+            { status: 200 }
+          );
+        /*
+        Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/DeleteOrganization
+        Show what evnt?.data sends from above resource
+        */
+      }
+    }
   } catch (error) {
     console.error("[LOG] Internal Server Error:", error);
     // Send the response
